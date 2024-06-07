cdf16a57ad65f2b4f3c4178ffda4aeb18f179faa
stelolololo <nathan.soleil@gmail.com>
2024-06-07 19:18:06 +0200

    firstPush

---
+++ index.js
@@ -0,0 +1,25 @@
+const navLogo = document.querySelector(".nav-logo"),
+navWrap = document.querySelector(".nav-wrap");
+
+function navLogoHover(){
+  navWrap.classList.toggle("nav-wrap-active");
+  console.log(navWrap.className);
+}
+
+function scrollToElement(element) {
+  if (element !== home) {
+    window.scrollTo({
+      top:
+        Math.round(
+          element.getBoundingClientRect().top +
+            document.documentElement.scrollTop
+        ) - 175,
+      behavior: "smooth",
+    });
+  }else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+}
